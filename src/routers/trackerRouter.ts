2ed750d3965ad570df3e00c5675501fc212b64c0
Mateus Borges <borgesm.work@gmail.com>
2023-01-23 17:48:55 -0300

    feat: trackerRouter (not implemented yet)

--- src/routers/trackerRouter.ts
+++ src/routers/trackerRouter.ts
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const router: Router = Router();
+
+router.get("/wishlist", (req: Request, res: Response) => res.sendStatus(501));
+router.post("/wishlist/:id", (req: Request, res: Response) => res.sendStatus(501));
+router.delete("/wishlist/:id", (req: Request, res: Response) => res.sendStatus(501));
 
 export default router;
