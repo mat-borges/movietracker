1d7dec827815cdbd93dc7092cf79d5570afa8e88
Mateus Borges <borgesm.work@gmail.com>
2023-01-23 17:43:32 -0300

    feat: auth router

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -6,7 +6,7 @@ import {
   signInSchemaValidation,
   signUpSchemaValidation,
   verifyUserCredentials,
-} from "../middlewares/authMiddlewares/index.js";
+} from "../middlewares/authMiddlewares/authMiddleware.js";
 import { signIn, signUp } from "../services/authServices.js";
 
 const router = Router();
@@ -15,4 +15,4 @@ router.post("/sign-up", signUpSchemaValidation, checkEmailExists, signUp);
 router.post("/sign-in", signInSchemaValidation, verifyUserCredentials, sessionExists, generateToken, signIn);
 router.delete("/sign-out", (req: Request, res: Response) => res.sendStatus(501));
 
-export const authRouter = router;
+export default router;
