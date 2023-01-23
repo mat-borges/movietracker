27eb47725a492bfec337e00a343cfadb9aa62d40
Mateus Borges <borgesm.work@gmail.com>
2023-01-23 17:48:26 -0300

    feat: movie router, auth router

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { json } from "express";
 
-import { authRouter } from "./routers/authRouter.js";
+import authRouter from "./routers/authRouter.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import moviesRouter from "./routers/moviesRouter.js";
 import { stripHtml } from "string-strip-html";
 
 export const cleanStringData = (string: any) => stripHtml(JSON.stringify(string)?.replace(/"|"/gi, ``)).result.trim();
@@ -11,9 +12,7 @@ dotenv.config();
 
 const server = express();
 
-server.use(cors()).use(json());
-
-server.use(authRouter);
+server.use(cors()).use(json()).use(authRouter).use(moviesRouter);
 
 const port: string = String(process.env.PORT);
 server.listen(port, () => console.log(`Running server on http://locahost:${port}`));
