a4bb2280caf42a85a52d157c302f3f4a5d84fd55
Mateus Borges <borgesm.work@gmail.com>
2023-01-27 05:03:20 -0300

    docs: change license to mozilla

--- dump.sql
+++ dump.sql
@@ -6,12 +6,10 @@ CREATE TABLE users (
 	"created_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE wishlist (
+CREATE TABLE tracker (
 	"id" SERIAL PRIMARY KEY NOT NULL,
 	"user_id" INTEGER NOT NULL,
 	"movie_id" INTEGER NOT NULL,
-	"rate" INTEGER NOT NULL,
-	"watched" BOOLEAN NOT NULL DEFAULT false,
 	"created_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
@@ -24,44 +22,36 @@ CREATE TABLE sessions (
 
 CREATE TABLE movies (
 	"id" SERIAL PRIMARY KEY NOT NULL,
-	"title" TEXT NOT NULL,
+	"title" TEXT NOT NULL UNIQUE,
 	"description" TEXT NOT NULL,
 	"year" INTEGER NOT NULL,
-	"director" INTEGER NOT NULL,
+	"director_id" INTEGER NOT NULL,
+	"poster" TEXT NOT NULL
 );
 
 CREATE TABLE genres (
 	"id" SERIAL PRIMARY KEY NOT NULL,
-	"name" TEXT NOT NULL
+	"name" TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE movie_genre (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"movie_id" INTEGER NOT NULL,
-	"genre_id" INTEGER NOT NULL
+CREATE TABLE "_GenresToMovies" (
+	"A" INTEGER NOT NULL,
+	"B" INTEGER NOT NULL
 );
 
 CREATE TABLE actors (
 	"id" SERIAL PRIMARY KEY NOT NULL,
-	"name" TEXT NOT NULL,
-	"birthday" TIMESTAMP NOT NULL
+	"name" TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE actor_movie (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"movie_id" INTEGER NOT NULL,
-	"actor_id" INTEGER NOT NULL
-);
-
-CREATE TABLE director_movie (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"movie_id" INTEGER NOT NULL,
-	"director_id" INTEGER NOT NULL
+CREATE TABLE "_ActorsToMovies" (
+	"A" INTEGER NOT NULL,
+	"B" INTEGER NOT NULL
 );
 
 CREATE TABLE directors (
 	"id" SERIAL PRIMARY KEY NOT NULL,
-	"name" TEXT NOT NULL
+	"name" TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE watched (
@@ -71,51 +61,23 @@ CREATE TABLE watched (
 	"created_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-ALTER TABLE "wishlist" ADD CONSTRAINT "wishlist_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-ALTER TABLE "wishlist" ADD CONSTRAINT "wishlist_fk1" FOREIGN KEY ("movie_id") REFERENCES "movies"("id");
+ALTER TABLE "tracker" ADD CONSTRAINT "tracker_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "tracker" ADD CONSTRAINT "tracker_fk1" FOREIGN KEY ("movie_id") REFERENCES "movies"("id");
 
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 
-ALTER TABLE "movies" ADD CONSTRAINT "movies_fk0" FOREIGN KEY ("director") REFERENCES "directors"("id");
+ALTER TABLE "movies" ADD CONSTRAINT "movies_fk0" FOREIGN KEY ("director_id") REFERENCES "directors"("id");
 
-ALTER TABLE "movie_genre" ADD CONSTRAINT "movie_genre_fk0" FOREIGN KEY ("movie_id") REFERENCES "movies"("id");
-ALTER TABLE "movie_genre" ADD CONSTRAINT "movie_genre_fk1" FOREIGN KEY ("genre_id") REFERENCES "genres"("id");
+ALTER TABLE "_GenresToMovies" ADD CONSTRAINT "_GenresToMovies_fk0" FOREIGN KEY ("A") REFERENCES "movies"("id");
+ALTER TABLE "_GenresToMovies" ADD CONSTRAINT "_GenresToMovies_fk1" FOREIGN KEY ("B") REFERENCES "genres"("id");
+CREATE UNIQUE INDEX "_GenresToMovies_AB_unique" ON "_GenresToMovies"("A" int4_ops,"B" int4_ops);
+CREATE INDEX "_GenresToMovies_B_index" ON "_GenresToMovies"("B" int4_ops);
 
-ALTER TABLE "actor_movie" ADD CONSTRAINT "actor_movie_fk0" FOREIGN KEY ("movie_id") REFERENCES "movies"("id");
-ALTER TABLE "actor_movie" ADD CONSTRAINT "actor_movie_fk1" FOREIGN KEY ("actor_id") REFERENCES "actors"("id");
+ALTER TABLE "_ActorsToMovies" ADD CONSTRAINT "_ActorsToMovies_fk0" FOREIGN KEY ("A") REFERENCES "movies"("id");
+ALTER TABLE "_ActorsToMovies" ADD CONSTRAINT "_ActorsToMovies_fk1" FOREIGN KEY ("B") REFERENCES "actors"("id");
+CREATE UNIQUE INDEX "_ActorsToMovies_AB_unique" ON "_ActorsToMovies"("A" int4_ops,"B" int4_ops);
+CREATE INDEX "_ActorsToMovies_B_index" ON "_ActorsToMovies"("B" int4_ops);
+
 
 ALTER TABLE "watched" ADD CONSTRAINT "watched_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 ALTER TABLE "watched" ADD CONSTRAINT "watched_fk1" FOREIGN KEY ("movie_id") REFERENCES "movies"("id");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
